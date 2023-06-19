165abab501ad552b7ea18557faf8f9b720c0b656
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-06-19 12:45:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // complete the given function
 
 function palindrome(str){
-
+ let rev = "";
+      Scanner sc = new Scanner(System.in);
+ 
+ 
+      let length = str.length();
+ 
+      for ( let i = length - 1; i >= 0; i-- )
+         rev = rev + str.charAt(i);
+ 
+      if (str.equals(rev)){
+       return "True";
+	  }
+      else{
+         return "False";
+ 
+   }
 }
+
 module.exports = palindrome
